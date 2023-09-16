36e173de28ec5d282ad0e5f7afd4f9275b8b87b3
Artur555999 <142072644+Artur555999@users.noreply.github.com>
2023-09-16 19:48:19 +0300

    JS

---
+++ app.js
@@ -0,0 +1,15 @@
+var myWord = prompt('Введите ваше имя');
+console.log('Меня зовут '+ myWord);
+var myWord = prompt ('Введите свой возрост');
+console.log('Мне ' + myWord + ' лет');
+var primer = prompt('Решите пример 6 + 3 = ?');
+console.log('Пример 1: 6 + 3 = 9  Ваш ответ: ' + primer);
+var primer = prompt('Решите пример: 15 - 4 = ?');
+console.log('Пример 2: 15 - 4 = 11  Ваш ответ: ' + primer);
+var primer = prompt('Решите пример: 8 * 0.5 = ?');
+console.log('Пример 3: 8 * 0.5 = 4  Ваш ответ: ' + primer)
+var primer = prompt('Решите пример: 50 / 10 = ?');
+console.log('Пример 4: 50 / 10 = 5  Ваш ответ: ' + primer);
+var primer = prompt('Решите пример: 16 / 5 = ?');
+console.log('Пример 5: 16 / 5 = 5(1)  Ваш ответ: ' + primer);
+alert('Нажмите для просмотра консоли');
